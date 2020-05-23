425ea01d5d7a8b0b716affef8d390340943a91ee
CraigTheCrag <craigkerr2509@gmail.com>
2020-05-23 16:56:08 +0100

    Turned on DEBUG Mode

--- website/settings.py
+++ website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+yw#wev948!tw2j8sksb%(tu07rzdfa#yn)&$ze&l3)m0ug4@j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", ".pythonanywhere.com"]
 
